f45861018897125e4d112213db0fba815c2ec1d6
fperez <franciscoperezldl@gmail.com>
2025-04-07 10:45:29 +0200

    Prueba de actualización automática con Netflify

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Football Daily 🚀 Automático</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
