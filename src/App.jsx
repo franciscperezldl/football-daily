66f4f162a41b5b65330e30881896b504e989d91d
fperez <franciscoperezldl@gmail.com>
2025-04-07 10:50:18 +0200

    Prueba de push automatico a Gitea y Github configurado

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <h1>Football Daily 🚀 Automático</h1>
+      <p>Esto es una app para ver los partidos del dia.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
